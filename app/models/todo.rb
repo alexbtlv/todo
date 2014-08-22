9c18f7bae0d749b55c1bd75be69e0c9102edadca
Alexander Batalov <alexbtlv@gmail.com>
2014-08-23 02:58:11 +0600

    initial devise setup

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,5 @@
 class Todo < ActiveRecord::Base
+	belongs_to :user
 
 	def toggle_completion!
 		update_attribute(:comleted, true)
---
+++ db/migrate/20140822205246_add_user_id_to_todo.rb
@@ -0,0 +1,6 @@
+class AddUserIdToTodo < ActiveRecord::Migration
+  def change
+  	add_column :todos, :user_id, :integer
+  	add_index :todos, :user_id
+  end
+end
