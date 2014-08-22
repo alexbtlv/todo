1aab0c3cdd940915f38d10ec5c5625e41fbe0672
Alexander Batalov <alexbtlv@gmail.com>
2014-08-23 01:59:38 +0600

    empty todos controller with the views

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,26 @@
+class TodosController < ApplicationController
+	
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
